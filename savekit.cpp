87934bbe40e3ce5dddfa31ece23cb88a2531c1d3
Chenlonggang <chenlonggang.love@163.com>
2014-06-20 17:58:36 +0800

    32bits only

--- savekit.cpp
+++ savekit.cpp
@@ -30,47 +30,56 @@ savekit::savekit(const char * file)
 		exit(0);
 	}
 }
- i32 savekit::writei64(i64 value)
+
+i32 savekit::writei64(i64 value)
 {
 	fwrite(&value,sizeof(i64),1,w);
 	return 1;
 }
- i32 savekit::writeu64(u64 value)
+
+i32 savekit::writeu64(u64 value)
 {
 	fwrite(&value,sizeof(u64),1,w);
 	return 1;
 }
- i32 savekit::writei32( i32 value)
+
+i32 savekit::writei32( i32 value)
 {
 	fwrite(&value,sizeof( i32),1,w);
 	return 1;
 }
- i32 savekit::writeu32(u32 value)
+
+i32 savekit::writeu32(u32 value)
 {
 	fwrite(&value,sizeof(u32),1,w);
 	return 1;
 }
- i32 savekit::writei16(i16 value)
+
+i32 savekit::writei16(i16 value)
 {
 	fwrite(&value,sizeof(i16),1,w);
 	return 1;
 }
- i32 savekit::writeu16(u16 value)
+
+i32 savekit::writeu16(u16 value)
 {
 	fwrite(&value,sizeof(u16),1,w);
 	return 1;
 }
- i32 savekit::writei64array(i64 * value,i32 len)
+
+i32 savekit::writei64array(i64 * value,i32 len)
 {
 	fwrite(value,sizeof(i64),len,w);
 	return 1;
 }
- i32 savekit::writeu64array(u64 * value,i32 len)
+
+i32 savekit::writeu64array(u64 * value,i32 len)
 {
 	fwrite(value,sizeof(u64),len,w);
 	return 1;
 }
- i32 savekit::writei32array(i32 * value,i32 len)
+
+i32 savekit::writei32array(i32 * value,i32 len)
 {
 	fwrite(value,sizeof(i32),len,w);
 	return 1;
